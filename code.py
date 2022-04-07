d8f0275aa0d1fa74b8f4349c9ef9429cfb186a86
Tiago de Paula <tiagodepalves@gmail.com>
2022-04-06 21:44:14 -0300

    Colors

--- code.py
+++ code.py
@@ -1,28 +1,37 @@
-from turtle import st
-from typing import Iterator, Literal, Optional
+from typing import Iterator, Literal
 from graphviz import Digraph
 
 
 Kind = Literal['tree', 'backward', 'forward', 'cross']
+
+color: dict[Kind, str] = {
+    'tree': 'black',
+    'backward': 'blue',
+    'forward': 'green',
+    'cross': 'red',
+}
 
 class Graph(Digraph):
     def __init__(self, name: str):
         super().__init__(name=name)
         self.label: dict[str, str] = {}
         self.nodes: dict[str, list[str]] = {}
-        self.links: dict[tuple[str, str], Optional[Kind]] = {}
+        self.links: dict[tuple[str, str], Kind | None] = {}
 
-    def node(self, name: str, label: str):
-        super().node(name, label)
+    def node(self, name: str, label: str, *, is_root: bool = False):
+        if is_root:
+            super().node(name, f'{label}({name})', color='darkgray', style='filled')
+        else:
+            super().node(name, f'{label}({name})')
         self.label[name] = label
         self.nodes[name] = []
 
-    def edge(self, tail: str, head: str):
-        super().edge(tail, head)
+    def edge(self, tail: str, head: str, kind: Kind | None = None):
         self.nodes[tail].append(head)
-        self.links[tail, head] = None
+        self.links[tail, head] = kind
+        super().edge(tail, head, color=color.get(kind, 'darkgray'))
 
-    def by_label(self, label: str) -> Optional[str]:
+    def by_label(self, label: str) -> str | None:
         for node, name in self.label.items():
             if name == label:
                 return node
@@ -66,7 +75,7 @@ def dfs(graph: Graph, initial: str) -> tuple[dict[str, int], dict[str, int], dic
 
     time = count()
 
-    def dfs_visit(node: str, parent: Optional[str] = None):
+    def dfs_visit(node: str, parent: str | None = None):
         if node in start:
             if not parent:
                 return
@@ -93,9 +102,23 @@ def dfs(graph: Graph, initial: str) -> tuple[dict[str, int], dict[str, int], dic
     return start, end, edges
 
 
+def colored_graph(graph: Graph, kind: dict[tuple[str, str], Kind], *, start: str | None = None) -> Graph:
+    colored = Graph(graph.name or "Colored")
+
+    for node in graph.nodes:
+        colored.node(node, graph.label[node], is_root=(node == start))
+
+    for tail, head in graph.links:
+        colored.edge(tail, head, kind.get((tail, head)))
+
+    return colored
+
+
 if __name__ == "__main__":
     graph = read()
     start, end, edges = dfs(graph, graph.by_label('Ti'))
+
+    graph = colored_graph(graph, edges, start=graph.by_label('Ti'))
     graph.render('out/graph.gv')
 
     for pair in graph.links:
